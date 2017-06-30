63c88350333f91b88ab3f3be622951bf6bf77d9c
Joshua Tessier <joshpc@gmail.com>
2017-06-30 20:43:03 +0900

    Removes reference to UIKit

--- Sources/AutoWebkit.h
+++ Sources/AutoWebkit.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 Joshua Tessier. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for AutoWebkit.
 FOUNDATION_EXPORT double AutoWebkitVersionNumber;
